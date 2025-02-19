3c207429f1eeddb52daf818e3e3578280ba34109
Patryk Kaźmierczak <patryk.kazmierczak@gazeta.pl>
2025-02-19 22:32:09 +0100

    Initial commit

---
+++ src/test/java/com/tcpmanager/tcpmanager/TcpManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcpmanager.tcpmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcpManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
