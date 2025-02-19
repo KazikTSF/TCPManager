ecbda5ea7c1984d9e2b34c9ab99d1e42469edee1
Patryk Kaźmierczak <patryk.kazmierczak@gazeta.pl>
2025-02-19 22:47:05 +0100

    Pipeline test

--- src/test/java/com/tcpmanager/tcpmanager/TcpManagerApplicationTests.java
+++ src/test/java/com/tcpmanager/tcpmanager/TcpManagerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TcpManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
